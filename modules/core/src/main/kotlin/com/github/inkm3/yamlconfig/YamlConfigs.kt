c88e198f6b21d1d40dd58f57d3d0f4266c5beffc
ink <inakami1214.ikachi@gmail.com>
2026-09-22 01:39:44 +0900

    feat: defaults指定時にCOPY_DEFAULTSをデフォルト化

--- modules/core/src/main/kotlin/com/github/inkm3/yamlconfig/YamlConfigs.kt
+++ modules/core/src/main/kotlin/com/github/inkm3/yamlconfig/YamlConfigs.kt
@@ -3,6 +3,7 @@ package com.github.inkm3.yamlconfig
 import com.github.inkm3.yamlconfig.save.YamlSaveMode
 import com.github.inkm3.yamlconfig.schema.YamlSchema
 import com.github.inkm3.yamlconfig.source.YamlInput
+import com.github.inkm3.yamlconfig.source.YamlMissingFilePolicy
 import com.github.inkm3.yamlconfig.source.YamlSource
 import com.github.inkm3.yamlconfig.spi.YamlEngine
 
@@ -12,6 +13,13 @@ public fun <T> yamlConfig(
     schema: YamlSchema<T>,
     defaultsSource: YamlInput? = null,
     saveMode: YamlSaveMode = YamlSaveMode.PRESERVE_OVERRIDES,
+    missingFilePolicy: YamlMissingFilePolicy = (
+        if (defaultsSource != null) {
+            YamlMissingFilePolicy.COPY_DEFAULTS
+        } else {
+            YamlMissingFilePolicy.DO_NOT_CREATE
+        }
+    ),
 ): YamlConfig<T> {
     return YamlConfig(
         engine,
@@ -19,5 +27,6 @@ public fun <T> yamlConfig(
         schema,
         defaultsSource,
         saveMode,
+        missingFilePolicy,
     )
 }
